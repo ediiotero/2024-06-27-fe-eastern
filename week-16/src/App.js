fe34657c3fd93c495e62e7a50e6c8ac047aaac3f
ediiotero <e.otero81@gmail.com>
2024-10-24 20:05:35 -0400

    week 16 done

--- week-16/src/App.js
+++ week-16/src/App.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import SiteNav from './components/SiteNav';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import User from './pages/User';
+import Error from './pages/Error';
+import { Info } from './pages/Info';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <Router>
+      <SiteNav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about'>
+          <Route index element={<About />} />
+          <Route path=':userid' element={<User />} />
+        </Route>
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/info' element={<Info />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
   );
 }
 
